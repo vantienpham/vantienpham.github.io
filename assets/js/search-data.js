461fa0f87bf76145f9d173a57f6ba142528edcd0
vantienpham <vantienpham.ai@gmail.com>
2025-12-29 13:51:37 +0000

    Deploying to gh-pages from @ vantienpham/vantienpham.github.io@e41ab81c439e6edf0fb6a9cd8f0b24e5b468e1c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -93,7 +93,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%76%61%6E%74%69%65%6E%70%68%61%6D.%61%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+          window.open("mailto:%76%61%6E.%74%69%65%6E.%70%68%61%6D@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
